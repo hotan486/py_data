b8fc5d30a9e25761360d7fd03b9b60ecf5be7f0d
name <hotan486@naver.com>
2019-07-13 17:20:59 +0900

    수정

--- lab/daproj/day2/ex09.py
+++ lab/daproj/day2/ex09.py
@@ -9,9 +9,18 @@ print(result)
 result = x1.multiply(3,4)
 
 print(result)
+data = [1,2]
+
+def s11(x,y):
+    return data+x+y
+
+print(s11(1,2))
 
 
 
+from util.calc import *
 
-
-
+result = plus(1,2)
+print(result)
+result = minus(1,2)
+print(result)
