d1455a4f31121299519e0ac9623c962b52cea5db
name <hotan486@naver.com>
2019-07-20 10:10:33 +0900

    dfg

--- lab/daproj/day2/ex09.py
+++ lab/daproj/day2/ex09.py
@@ -1,26 +1,26 @@
-import util.calc as c
-
-
-from util import calc as x
-from util import calc2 as x1
-result = x.minus(1,2)
-print(result)
-
-result = x1.multiply(3,4)
-
-print(result)
-data = [1,2]
-
-def s11(x,y):
-    return data+x+y
-
-print(s11(1,2))
-
-
-
-from util.calc import *
-
-result = plus(1,2)
-print(result)
-result = minus(1,2)
-print(result)
+# import util.calc as c
+#
+#
+# from util import calc as x
+# from util import calc2 as x1
+# result = x.minus(1,2)
+# print(result)
+#
+# result = x1.multiply(3,4)
+#
+# print(result)
+# data = [1,2]
+#
+# def s11(x,y):
+#     return data+x+y
+#
+# print(s11(1,2))
+#
+#
+#
+# from util.calc import *
+#
+# result = plus(1,2)
+# print(result)
+# result = minus(1,2)
+# print(result)
---
+++ lab/daproj/day2/ex99.py
@@ -0,0 +1,4 @@
+import collections
+num_firends = [100,40,30,54,25,3,100,100,100,3,3]
+friend_counts = collections.Counter(num_firends)
+print('friends:', friend_counts)
